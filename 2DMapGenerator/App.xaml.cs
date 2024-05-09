85f3baea2842a23215bb23f10f2233217c892b85
roland31x <roland31x@gmail.com>
2024-05-09 21:58:32 +0300

    add base ui

--- 2DMapGenerator/App.xaml.cs
+++ 2DMapGenerator/App.xaml.cs
@@ -43,6 +43,22 @@ namespace _2DMapGenerator
         {
             m_window = new MainWindow();
             m_window.Activate();
+            m_window.AppWindow.Resize(new Windows.Graphics.SizeInt32(1000,800));
+            m_window.AppWindow.Title = "2D Map Generator";
+            var hWnd = WinRT.Interop.WindowNative.GetWindowHandle(m_window);
+            Microsoft.UI.WindowId windowId = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(hWnd);
+            Microsoft.UI.Windowing.AppWindow appWindow = Microsoft.UI.Windowing.AppWindow.GetFromWindowId(windowId);
+            if (appWindow is not null)
+            {
+                Microsoft.UI.Windowing.DisplayArea displayArea = Microsoft.UI.Windowing.DisplayArea.GetFromWindowId(windowId, Microsoft.UI.Windowing.DisplayAreaFallback.Nearest);
+                if (displayArea is not null)
+                {
+                    var CenteredPosition = appWindow.Position;
+                    CenteredPosition.X = ((displayArea.WorkArea.Width - appWindow.Size.Width) / 2);
+                    CenteredPosition.Y = ((displayArea.WorkArea.Height - appWindow.Size.Height) / 2);
+                    appWindow.Move(CenteredPosition);
+                }
+            }
         }
 
         private Window m_window;
