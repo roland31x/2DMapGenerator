d937c9e6497945b2b99aeb82fbfa87ea9ff009ca
roland31x <roland31x@gmail.com>
2024-05-13 13:24:15 +0300

    push

--- 2DMapGenerator/PerlinNoise.cs
+++ 2DMapGenerator/PerlinNoise.cs
@@ -73,8 +73,8 @@ namespace _2DMapGenerator
         }
         public float ComputePerlinNoise(float x, float y)
         {
-            int X = (int)Math.Floor(x) % 255;
-            int Y = (int)Math.Floor(y) % 255;
+            int X = (int)Math.Floor(x) & 255;
+            int Y = (int)Math.Floor(y) & 255;
 
             double xf = x - Math.Floor(x);
             double yf = y - Math.Floor(y);
@@ -88,17 +88,17 @@ namespace _2DMapGenerator
             Vector2 bottomRight = new Vector2(xf - 1.0, yf);
             Vector2 bottomLeft = new Vector2(xf, yf);
 
-            int tr = permutation[X + 1] + Y + 1;
-            Vector2 constantVectorTopRight = GetConstantVector(permutation[tr % 255]);
+            int tr = permutation[(X + 1) & 255] + Y + 1;
+            Vector2 constantVectorTopRight = GetConstantVector(permutation[tr & 255]);
 
             int tl = permutation[X] + Y + 1;
-            Vector2 constantVectorTopLeft = GetConstantVector(permutation[tl % 255]);
+            Vector2 constantVectorTopLeft = GetConstantVector(permutation[tl & 255]);
 
-            int br = permutation[X + 1] + Y;
-            Vector2 constantVectorBottomRight = GetConstantVector(permutation[br % 255]);
+            int br = permutation[(X + 1) & 255] + Y;
+            Vector2 constantVectorBottomRight = GetConstantVector(permutation[br & 255]);
 
             int bl = permutation[X] + Y;
-            Vector2 constantVectorBottomLeft = GetConstantVector(permutation[bl % 255]);
+            Vector2 constantVectorBottomLeft = GetConstantVector(permutation[bl & 255]);
 
             double dotTopRight = topRight.Dot(constantVectorTopRight);
             double dotTopLeft = topLeft.Dot(constantVectorTopLeft);
@@ -134,22 +134,15 @@ namespace _2DMapGenerator
             return result;
         }
 
-        public Task<Map> ComputePerlinNoiseMapAsync(int width, int height, int smoothness)
+        public Task<Map> ComputePerlinNoiseMapAsync(int width, int height, int roughness)
         {
             Map map = new Map(width, height);
-
-            float minsofar = float.MaxValue;
-            float maxsofar = float.MinValue;
 
             Parallel.For(0, height, y =>
             {
                 for (int x = 0; x < width; x++)
                 {
-                    map[x, y] = FractalBrownianMotion(x, y, smoothness);
-                    if (map[x, y] < minsofar)
-                        minsofar = map[x, y];
-                    if (map[x, y] > maxsofar)
-                        maxsofar = map[x, y];
+                    map[x, y] = FractalBrownianMotion(x, y, roughness);
                 }
             });
 
